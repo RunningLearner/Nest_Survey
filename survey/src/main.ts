b3434c80da96f9f078723b9507a2ac68ec62a88d
RunningLearner <tkacnsdms@naver.com>
2023-01-10 17:26:03 +0900

    feat: 윈스턴 로거 적용

--- survey/src/main.ts
+++ survey/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { winstonLogger } from './utils/winston.util';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: winstonLogger,
+  });
   await app.listen(4000);
 }
 bootstrap();
